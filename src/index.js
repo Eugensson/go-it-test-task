d8e8091e2eaf70cd0705079ede157050f3b2ccc0
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-06-07 13:48:34 +0300

    Bagfix

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/tweets-cards-app">
+        <BrowserRouter basename="/go-it-test-task">
           <App />
         </BrowserRouter>
       </PersistGate>
